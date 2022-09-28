530041acbc00543f06c5f6b7f8f16dde6c87a2bd
lurbano <lurbano@earthsciweb.org>
2022-09-28 13:35:46 -0500

    ellipse translation

--- ellipseTest.py
+++ ellipseTest.py
@@ -1,7 +1,11 @@
 from svgInator import *
 
-s = svgInator("ellipseTest.svg")
+s = svgInator("ellipseTest.svg", units="")
+rx = 10
+ry = 20
 
-s.ellipse()
-s.ellipse(transform='rotate(90)')
+for i in range(10):
+    x = i * 2.1* ry
+    s.ellipse(rx=rx, ry=ry, pos=vector(x,0,0))
+    s.ellipse(rx=rx, ry=ry, transform=f'translate({x},0), rotate(90)')
 s.close()
